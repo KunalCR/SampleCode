5841870fabda3b62931f3f5c496dd7a01b11c697
anjalisheel <anjalisheel@gmail.com>
2021-09-20 10:35:16 +0530

    Add files via upload
    Find the Missing Number in an array

---
+++ SampleCode/MissingValueInArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //Link - https://www.geeksforgeeks.org/find-the-missing-number/
+    public class Program
+    {
+        static int GetMissingValue(int[] arr)
+        {
+            int n = arr.Length;
+            int total;
+
+            total = ((n + 1) * (n + 2)) / 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                total -= arr[i];
+            }
+
+            return total;
+        }
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 5, 6, 3, 7 };
+            Console.WriteLine("Missing value is " + GetMissingValue(arr));
+            Console.ReadLine();
+        }
+    }
+}
